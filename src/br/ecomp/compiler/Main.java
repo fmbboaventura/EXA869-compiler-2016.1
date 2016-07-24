c977c7f9690a0ca180098ed8bc263dc66919e748
Filipe Boaventura <fmbboaventura@gmail.com>
2016-07-23 21:59:35 -0300

    Tentativa de implementar o identificador de lexemas.

--- src/br/ecomp/compiler/Main.java
+++ src/br/ecomp/compiler/Main.java
@@ -1,23 +1,29 @@
 package br.ecomp.compiler;
 
+import br.ecomp.compiler.lexer.Lexer;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Filipe Boaventura
  * @since 16/07/2016.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         final File input;
 
         if(args.length > 0) input = new File(args[0]);
         else input = new File("input");
 
+        Lexer lexer = new Lexer();
+
         if (input.isDirectory())
             for (File file : input.listFiles()) {
                 // Chamar o lexer para cada arquivo da pasta
                 System.out.println(file.getName());
+                lexer.createTokens(file);
             }
     }
 }
--- src/br/ecomp/compiler/lexer/Lexer.java
+++ src/br/ecomp/compiler/lexer/Lexer.java
@@ -1,12 +1,91 @@
 package br.ecomp.compiler.lexer;
 
+import java.io.*;
+import java.util.LinkedList;
+
 /**
  * @author Filipe Boaventura
  * @since 20/07/2016.
  */
 public class Lexer {
 
-    public void createTokens() {
+    public void createTokens(File input) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        LinkedList<String> lexemeList = new LinkedList<>();
+        boolean readingString = false;
+        String lexeme = "";
 
+        char c;
+        int i;
+        while ((i = reader.read()) != -1) {
+            c = (char) i;
+
+            if(isLexDelimiter(c, readingString)){
+                if (!lexeme.isEmpty() && !readingString) {
+                    lexemeList.add(lexeme);
+                    lexeme = Character.isWhitespace(c) ? "" : Character.toString(c);
+                }
+
+                switch (c){
+                    case '"':
+                        if(readingString) {
+                            lexeme += c;
+                            lexemeList.add(lexeme);
+                            lexeme = "";
+                        }
+                        readingString = !readingString;
+                        break;
+                    case '>':
+                        reader.mark(1);
+                        c = (char) reader.read();
+                        if (c == '=') lexeme += c;
+                        else reader.reset();
+                        lexemeList.add(lexeme);
+                        lexeme = "";
+                        break;
+                    case '<':
+                        reader.mark(1);
+                        c = (char) reader.read();
+                        if (c == '=' || c == '>') lexeme += c;
+                        else reader.reset();
+                        lexemeList.add(lexeme);
+                        lexeme = "";
+                        break;
+                    case '-':
+                        reader.mark(1);
+                        c = (char) reader.read();
+                        if (Character.isDigit(c)) lexeme += c;
+                        else reader.reset();
+                        lexemeList.add(lexeme);
+                        lexeme = "";
+                        break;
+                }
+
+                continue;
+            }
+
+            lexeme += c;
+        }
+        for (String s :
+                lexemeList) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * Retorna se o caractere é um delimitador de lexema.
+     */
+    private boolean isLexDelimiter(char c, boolean readingString) {
+        if (readingString) return (c == '"') || (c == '\n') || (c == '\r');
+        return (Character.isWhitespace(c) ||
+                (c == '<') ||
+                (c == '>') ||
+                (c == '+') ||
+                (c == '-') ||
+                (c == '*') ||
+                (c == '/') ||
+                (c == ';') ||
+                (c == '"') ||
+                (c == '\''));
     }
 }
