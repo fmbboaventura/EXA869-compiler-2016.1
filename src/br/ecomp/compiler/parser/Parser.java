1a367d3758fd0d36246943eea7c3bb715c1b1aef
Filipe Boaventura <fmbboaventura@gmail.com>
2016-09-25 18:12:01 -0300

    Comentários no Parser

--- src/br/ecomp/compiler/parser/Parser.java
+++ src/br/ecomp/compiler/parser/Parser.java
@@ -10,10 +10,19 @@ import java.util.Iterator;
  * @since 24/09/2016.
  */
 public class Parser {
+    /**
+     * Ponteiro para o simbolo atual da entrada.
+     */
     private Token currentToken;
     private Iterator<Token> tokenIt;
     private int errorCount;
 
+    /**
+     * Inicia a análise sintática sobre a coleção de
+     * {@link Token}s.
+     * @param tokens coleção de entrada contendo os
+     * {@link Token}s para a análise sintática.
+     */
     public void parse(Collection<Token> tokens) {
         errorCount = 0;
         tokenIt = tokens.iterator();
@@ -22,11 +31,26 @@ public class Parser {
         if (errorCount == 0) System.out.println("\tAnalise Sintatica concluida com sucesso.");
     }
 
+    /**
+     * Consome um simbolo do vetor de entrada e atualiza o
+     * {@link Parser#currentToken}. Consome mais um simbolo
+     * caso um token do tipo {@link br.ecomp.compiler.lexer.Token.TokenType#COMMENT}
+     * seja encontrado, para ignorar comentários.
+     */
     private void nextToken() {
         if (tokenIt.hasNext()) currentToken = tokenIt.next();
         accept(Token.TokenType.COMMENT); // Pulando comentarios
     }
 
+    /**
+     * Verifica se o {@link Parser#currentToken} é do tipo
+     * informado por parametro. Caso seja, move o ponteiro
+     * para o próximo simbolo da entrada.
+     * @param type o {@link br.ecomp.compiler.lexer.Token.TokenType}
+     *             que o {@link Parser#currentToken} deve apresentar.
+     * @return true, caso o simbolo atual apresente este tipo. false,
+     *          caso contrário.
+     */
     private boolean accept (Token.TokenType type) {
         if (currentToken.getType() == type) {
             nextToken();
@@ -34,6 +58,16 @@ public class Parser {
         } return false;
     }
 
+    /**
+     * Espera que o {@link Parser#currentToken} seja do
+     * {@link br.ecomp.compiler.lexer.Token.TokenType}
+     * informado por parametro. Incrementa a contagem de
+     * erros e exibe mensagem de erro caso não seja.
+     * @param type o {@link br.ecomp.compiler.lexer.Token.TokenType}
+     *             que o {@link Parser#currentToken} deve apresentar.
+     * @return true, caso o simbolo atual apresente este tipo. false,
+     *          caso contrário.
+     */
     private boolean expect(Token.TokenType type) {
         if (accept(type)) return true;
         error("Erro! Token inesperado :"
@@ -51,12 +85,14 @@ public class Parser {
      *            Nao-Terminais
      *****************************************/
 
+    // <Programa> ::= <Variaveis><C>|<C>
     private void programa() {
         nextToken();
         variaveis();
         c();
     }
 
+    // <Variaveis> ::= 'var''inicio'<Var_List>'fim'
     private void variaveis() {
         if (accept(Token.TokenType.VAR)){ // Se aceitou um var
             expect(Token.TokenType.INICIO); // Espera um inicio
@@ -77,6 +113,7 @@ public class Parser {
     }
 
     // <Bloco> ::= 'inicio' 'fim'
+    // bloco simplificado por agora
     private void bloco() {
         expect(Token.TokenType.INICIO);
         expect(Token.TokenType.FIM);
@@ -116,6 +153,7 @@ public class Parser {
         }
     }
 
+    // <Var_List> ::= <Tipo><Var_Decl><Var_List> |<>
     private void varlist() {
         if (accept(Token.TokenType.DATA_TYPE)) { // espera um tipo
             vardecl();
@@ -133,11 +171,17 @@ public class Parser {
         }
     }
 
+    // <Id_Vetor> ::= id<Vetor>
     private void idvetor() {
         expect(Token.TokenType.IDENTIFIER);
         vetor();
     }
 
+    // <Vetor> ::= '<<<'numero_t<Vetor2>'>>>'  | <>
+    // vetor simplificado.
+    // Vetor original:
+    // <Vetor> ::= '<<<'<Exp_Aritmetica><Vetor2>'>>>'  | <>
+    // <Vetor2> ::= ','<Exp_Aritmetica><Vetor2> | <>
     private void vetor() {
         if (accept(Token.TokenType.VEC_DELIM_L)) {//se encontrou <<<
             expect(Token.TokenType.NUMBER); // TODO implementar as expressoes
@@ -146,6 +190,7 @@ public class Parser {
         }
     }
 
+    // <Vetor2> ::= ','numero_t<Vetor2> | <>
     private void vetor2() {
         if (accept(Token.TokenType.COMMA)) { // se encontrou uma virgula
             expect(Token.TokenType.NUMBER); // tem que encontrar um numero em seguida
